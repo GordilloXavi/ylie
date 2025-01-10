1007189413885f80a421aa8905a72eedde227fda
xavi <xavigr00@gmail.com>
2025-01-10 18:46:44 +0100

    add colors to stems

--- src/Experience/World/StemObjectGroup.js
+++ src/Experience/World/StemObjectGroup.js
@@ -2,11 +2,11 @@ import * as THREE from 'three'
 import Experience from "../Experience"
 
 class StemObject {
-    constructor (audio_buffer, position, audioListener) { ///, position, color) { // TODO: add position and color
+    constructor (audio_buffer, position, color) { ///, position, color) { // TODO: add position and color
         this.experience = new Experience()
         this.auaudio_buffer = audio_buffer
 
-        this.mesh = this.createMesh()
+        this.mesh = this.createMesh(color)
         this.mesh.position.set(position.x, position.y, position.z)
 
         this.positionalSound = new THREE.PositionalAudio(this.experience.camera.audioListener)
@@ -24,9 +24,9 @@ class StemObject {
         return this.positionalSound.isPlaying
     }
 
-    createMesh () {
+    createMesh (color) {
         const geometry = new THREE.BoxGeometry(.3, .3, .3)
-        const material = new THREE.MeshBasicMaterial({ color: 0xa0ffa0 })
+        const material = new THREE.MeshBasicMaterial({ color: color })
         const cube = new THREE.Mesh(geometry, material)
 
         return cube
@@ -47,17 +47,24 @@ export default class StemObjectGroup {
         // TODO: here we will need to pass arguments with the audio paths, the positions and the models
         this.experience = new Experience()
         const objectHeight = 0.5
-        const bass_stem_position = new THREE.Vector3(5, objectHeight, 0)
-        const drums_stem_position = new THREE.Vector3(1.545, objectHeight, 4.755)
-        const guitars_stem_position = new THREE.Vector3(-4.045, objectHeight, 2.938)
-        const synths_stem_position = new THREE.Vector3(-4.045, objectHeight, -2.938)
-        const vocals_stem_position = new THREE.Vector3(1.545, objectHeight, -4.755)
 
-        this.bass_stem_object = new StemObject(this.experience.resources.items.soul_net_bass, bass_stem_position)
-        this.drums_stem_object = new StemObject(this.experience.resources.items.soul_net_drums, drums_stem_position)
-        this.guitars_stem_object = new StemObject(this.experience.resources.items.soul_net_guitars, guitars_stem_position)
-        this.synths_stem_object = new StemObject(this.experience.resources.items.soul_net_synths, synths_stem_position)
-        this.vocals_stem_object = new StemObject(this.experience.resources.items.soul_net_vocals, vocals_stem_position)
+        const bass_stem_position = new THREE.Vector3(2, objectHeight, -11)
+        const drums_stem_position = new THREE.Vector3(0, objectHeight, -11)
+        const guitars_stem_position = new THREE.Vector3(-4, objectHeight, -11)
+        const synths_stem_position = new THREE.Vector3(4, objectHeight, -11)
+        const vocals_stem_position = new THREE.Vector3(-2, objectHeight, -11)
+
+        const bass_stem_color = 0x00ff00
+        const drums_stem_color = 0xffffff
+        const guitars_stem_color = 0xff0000
+        const synths_stem_color = 0x0000ff
+        const vocals_stem_color = 0xffff00
+
+        this.bass_stem_object = new StemObject(this.experience.resources.items.soul_net_bass, bass_stem_position, bass_stem_color)
+        this.drums_stem_object = new StemObject(this.experience.resources.items.soul_net_drums, drums_stem_position, drums_stem_color)
+        this.guitars_stem_object = new StemObject(this.experience.resources.items.soul_net_guitars, guitars_stem_position, guitars_stem_color)
+        this.synths_stem_object = new StemObject(this.experience.resources.items.soul_net_synths, synths_stem_position, synths_stem_color)
+        this.vocals_stem_object = new StemObject(this.experience.resources.items.soul_net_vocals, vocals_stem_position, vocals_stem_color)
 
         this.experience.scene.add(this.bass_stem_object.mesh)
         this.experience.scene.add(this.drums_stem_object.mesh)
