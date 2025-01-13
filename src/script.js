4d23141642d9abaac42df03ad4bbafceb15353cb
xavi <xavigr00@gmail.com>
2025-01-13 01:20:52 +0100

    mobile device blocker

--- src/script.js
+++ src/script.js
@@ -1,4 +1,12 @@
 
 import Experience from './Experience/Experience.js'
 
-const experience = new Experience(document.querySelector('canvas.webgl'))
+// Check for mobile devices
+const isMobile = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop|BlackBerry|Mobile/i.test(navigator.userAgent) || window.matchMedia("(max-width: 768px)").matches
+
+if (isMobile) {
+    const banner = document.getElementById('mobile-banner')
+    banner.style.display = 'flex'
+} else {
+    const experience = new Experience(document.querySelector('canvas.webgl'))
+}
