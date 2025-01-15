4265038b3d7c284b001340c8d542085a66ec60ec
xavi <xavigr00@gmail.com>
2025-01-15 02:04:53 +0100

    add logs

--- src/Experience/YlieFirstPersonControls.js
+++ src/Experience/YlieFirstPersonControls.js
@@ -71,6 +71,7 @@ export default class YlieFirstPersonControls
         this.experience.world.handleClick(event)
 
         this.experience.socket.emit('update_position', { id: 333, position: 'awesome' })
+        console.log('emit update_position')
     }
 
     handleKeyDown = (event) => {
