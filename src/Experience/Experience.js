c2e129e0638521390f080eae64188b70d2ea0353
xavi <xavigr00@gmail.com>
2025-01-15 02:11:24 +0100

    add port again

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -58,7 +58,7 @@ export default class Experience
     }
 
     initSockets() {
-        this.socket = io('https://seal-app-hivs4.ondigitalocean.app')
+        this.socket = io('https://seal-app-hivs4.ondigitalocean.app:5000')
         console.log('init socket')
 
         this.socket.on('update_position', (data) => {
